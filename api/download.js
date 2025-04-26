411300ea668b7c58c122e717af2114bae3bef9af
roy01 <subhamroy1180@gmail.com>
2025-04-26 13:09:19 +0530

    rename file name

--- api/download.js
+++ api/download.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const filePath = './public/firmware_v1.bin';
 
-    res.setHeader('Content-Disposition', 'attachment; filename="firmware.bin"');
+    res.setHeader('Content-Disposition', 'attachment; filename="firmware_v1.bin"');
     res.setHeader('Content-Type', 'application/octet-stream');
 
     // Read file from filesystem
